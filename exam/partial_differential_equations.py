ddacc977f611419debd0555fe35998f655f93430
JP Duminy <jp@duminy.org>
2026-05-11 10:20:36 +0100

    Finished engine room by adding functions for the boundary value problems.

--- exam/partial_differential_equations.py
+++ exam/partial_differential_equations.py
@@ -168,3 +168,126 @@ def compute_curl_3D(fx: np.ndarray, fy: np.ndarray, fz: np.ndarray, N: int, dx: 
                                  (fx[i,(j+1)%N,k] - fx[i,(j-1)%N,k])) / (2*dx))
                 
     return curl_x, curl_y, curl_z
+
+@njit(parallel=True)
+def jacobi_3D_dirichlet(f0: np.ndarray, rho: np.ndarray, dx: float, N: int) -> np.ndarray:
+    """
+    Jacobi algorithm for a quantity in 3D with Dirichlet BCs.
+    """
+    f_new = np.zeros_like(f0) # zeros_like instead of empty_like applies dirichlet intrinsically
+    # range accomodates dirichlet BCs: edges = 0 always
+    for i in prange(1, N-1): # only do prange for outer loop 
+        for j in range(1, N-1):
+            for k in range(1, N-1):
+                f_new[i,j,k] = ((f0[i+1,j,k] + f0[i-1,j,k]
+                              + f0[i,j+1,k] + f0[i,j-1,k]
+                              + f0[i,j,k+1] + f0[i,j,k-1])
+                              + dx**2 * rho[i,j,k]) / 6 # 6 nearest neighbours
+    return f_new
+
+@njit(parallel=True)
+def jacobi_2D_dirichlet(f0: np.ndarray, rho: np.ndarray, dx: float, N: int) -> np.ndarray:
+    """
+    Jacobi algorithm for a quantity in 2D with Dirichlet BCs.
+    """
+    f_new = np.zeros_like(f0) 
+    for i in prange(1, N-1):
+        for j in range(1, N-1):
+
+            f_new[i,j] = ((f0[i+1,j] + f0[i-1,j]
+                            + f0[i,j+1] + f0[i,j-1])
+                            + dx**2 * rho[i,j]) / 4 # 4 nearest neighbours
+    return f_new
+
+@njit
+def gauss_seidel_3D_dirichlet(f: np.ndarray, rho: np.ndarray, dx: float, N: int, 
+                              omega: float = 1) -> None:
+    """
+    Gauss-Seidel algorithm for quantity in 3D with Dirichlet BCs.
+    Omega controls successive over-relaxation (default 1, reduces to Gauss-Seidel).
+    Sequential, do not parallelise.
+    """
+    for i in range(1, N-1):
+        for j in range(1, N-1):
+            for k in range(1, N-1):
+
+                f_old = f[i,j,k]
+                f_gs = ((f[i+1,j,k] + f[i-1,j,k]
+                              + f[i,j+1,k] + f[i,j-1,k]
+                              + f[i,j,k+1] + f[i,j,k-1])
+                              + dx**2 * rho[i,j,k]) / 6
+                
+                f[i,j,k] = omega * f_gs + (1 - omega)*f_old
+
+@njit
+def gauss_seidel_2D_dirichlet(f: np.ndarray, rho: np.ndarray, dx: float, N: int, 
+                              omega: float = 1) -> None:
+    """
+    Gauss-Seidel algorithm for quantity in 2D with Dirichlet BCs.
+    Omega controls successive over-relaxation (default 1, reduces to Gauss-Seidel).
+    Sequential, do not parallelise.
+    """
+    for i in range(1, N-1):
+        for j in range(1, N-1):
+            f_old = f[i,j]
+            f_gs = ((f[i+1,j] + f[i-1,j]
+                        + f[i,j+1] + f[i,j-1])
+                        + dx**2 * rho[i,j]) / 4
+            f[i,j] = omega * f_gs + (1 - omega)*f_old
+
+def apply_dirichlet_bc_2D(f: np.ndarray, val: float = 0.0) -> None:
+    """
+    Applies the Dirichlet boundary condition to a 2D array: boundaries = val.
+    The default value is 0.
+    """
+    f[0,:] = val
+    f[-1,:] = val
+    f[:,0] = val
+    f[:,-1] = val
+
+def apply_dirichlet_bc_3D(f: np.ndarray, val: float = 0.0) -> None:
+    """
+    Applies the Dirichlet boundary condition to a 3D array: boundaries = val.
+    The default value is 0.
+    """
+    f[0,:,:] = val
+    f[-1,:,:] = val
+    f[:,0,:] = val
+    f[:,-1,:] = val
+    f[:,:,0] = val
+    f[:,:,-1] = val
+
+def apply_neumann_bc_2D(f: np.ndarray, dx: float, val: float = 0.0) -> None:
+    """
+    Applies the Neumann boundary condition to a 2D array: derivate at boundaries = val.
+    The default value is 0.
+    """
+    f[0,:] = f[1,:] + val*dx 
+    f[-1,:] = f[-2,:] + val*dx 
+    f[:,0] = f[:,1] + val*dx
+    f[:,-1] = f[:,-2] + val*dx 
+
+def apply_neumann_bc_3D(f: np.ndarray, dx: float, val: float = 0.0) -> None:
+    """
+    Applies the Neumann boundary condition to a 3D array: derivate at boundaries = val.
+    The default value is 0.
+    """
+    f[0,:,:] = f[1,:,:] + val*dx 
+    f[-1,:,:] = f[-2,:,:] + val*dx 
+    f[:,0,:] = f[:,1,:] + val*dx
+    f[:,-1,:] = f[:,-2,:] + val*dx 
+    f[:,:,0] = f[:,:,1] + val*dx 
+    f[:,:,-1] = f[:,:,-2] + val*dx
+
+def spatial_integral(f: np.ndarray, dx: float) -> float:
+    """
+    Integrates over the grid by approximating each cell's area.
+    Automatically handles the number of dimensions.
+    """
+    return np.sum(f) * dx**f.ndim
+
+def find_stable_timestep(D: float, dx: float, ndim: int = 2, safety_threshold: float = 0.9):
+    """
+    Uses the Von Neumann analysis to find a (somewhat conservative) stable timestep for desired parameters.
+    """
+    return safety_threshold * dx**2 / (2 * ndim * D)
